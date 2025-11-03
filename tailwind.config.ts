cabd3c6e35afc1762225e513e2d17a54369bc8ba
Teun de Jong <teundejong52@gmail.com>
2025-11-03 21:02:27 +1030

    fix: tailwind darkMode type

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,15 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: ["class"],
+  darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 };
+
 export default config;
